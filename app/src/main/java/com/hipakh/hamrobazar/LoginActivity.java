91cc68b7610763fd480e3218289832d058c7d9ff
hipak <hpk393@gmail.com>
2020-01-16 08:00:10 +0545

    User Model Created and Login & Register Activity added

--- app/src/main/java/com/hipakh/hamrobazar/LoginActivity.java
+++ app/src/main/java/com/hipakh/hamrobazar/LoginActivity.java
@@ -19,5 +19,84 @@ import com.hipakh.hamrobazar.BLL.LoginBLL;
 import com.hipakh.hamrobazar.StrictModeClass.StrictModeClass;
 
 public class LoginActivity extends AppCompatDialogFragment {
+    private EditText etEmail, etPassword;
 
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext(),R.style.Dialog);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_login, null);
+
+
+        builder.setView(view)
+                .setTitle("Login")
+
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(getContext(), DashboardActivity.class);
+                        getContext().startActivity(intent);
+
+
+                    }
+                })
+
+                .setPositiveButton("Login", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        if (TextUtils.isEmpty(etEmail.getText()))
+                        {
+                            etEmail.setError("Enter email address");
+                            etEmail.requestFocus();
+                            return;
+                        }
+                        if (TextUtils.isEmpty(etPassword.getText()))
+                        {
+                            etPassword.setError("Enter password");
+                            etPassword.requestFocus();
+                            return;
+                        }
+
+                        String email = etEmail.getText().toString();
+                        String pwd = etPassword.getText().toString();
+
+                        LoginBLL loginBLL = new LoginBLL();
+                        StrictModeClass.StrictMode();
+
+                        if (loginBLL.checkUser(email, pwd)) {
+                            SharedPreferences sharedPreferences = getActivity().getSharedPreferences("User", Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putString("email", etEmail.getText().toString());
+                            editor.putString("password", etPassword.getText().toString());
+                            editor.commit();
+                            Toast.makeText(getActivity(), "Login Sucessfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getContext(), DashboardActivity.class);
+                            getContext().startActivity(intent);
+
+                        } else {
+                            Toast.makeText(getActivity(), "Email or password donot match" + email + pwd, Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                })
+
+                .setNeutralButton("Register here", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(getContext(), RegisterActivity.class);
+                        getContext().startActivity(intent);
+
+                    }
+                });
+
+
+        etEmail = view.findViewById(R.id.etEmail);
+        etPassword = view.findViewById(R.id.etPassword);
+
+        return builder.create();
+
+    }
 }
--- app/src/main/java/com/hipakh/hamrobazar/ServerResponse/ImageResponse.java
+++ app/src/main/java/com/hipakh/hamrobazar/ServerResponse/ImageResponse.java
@@ -1,5 +1,17 @@
 package com.hipakh.hamrobazar.ServerResponse;
 
 public class ImageResponse {
+    private String filename;
 
+    public ImageResponse(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
 }
